e0f0c15e9bf9a7ad909ac894b365250acc6d2cff
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2019-01-29 13:02:49 -0800

    Parameterized AKS Manifest Repo

--- gitops/azure-devops/build.sh
+++ gitops/azure-devops/build.sh
@@ -1,7 +1,6 @@
 cd /home/vsts/work/1/s/
 
-# Store the ouput of `curl -s https://api.github.com/repos/Microsoft/fabrikate/tags`
-# If the release number is not provided, then download the latest
+# If the version number is not provided, then download the latest
 if [ -z "$VERSION" ]
 then
     VERSIONS=$(curl -s https://api.github.com/repos/Microsoft/fabrikate/tags)
@@ -13,23 +12,25 @@ fi
 
 echo "Downloading Fabrikate..."
 echo "Latest Fabrikate Version: $LATEST_VERSION"
-#wget "https://github.com/Microsoft/fabrikate/releases/download/0.1.2/fab-v0.1.2-linux-amd64.zip"
 wget "https://github.com/Microsoft/fabrikate/releases/download/$LATEST_VERSION/fab-v$LATEST_VERSION-linux-amd64.zip"
-#unzip fab-v0.1.2-linux-amd64.zip -d fab
 unzip fab-v$LATEST_VERSION-linux-amd64.zip -d fab
 export PATH=$PATH:/home/vsts/work/1/s/fab
 fab install
 
-#git clone https://github.com/Microsoft/fabrikate
-#cd fabrikate/examples/getting-started
 fab generate prod
 echo "FAB GENERATE PROD COMPLETED"
 ls -a
 
+# Git
 cd /home/vsts/work/1/s/
 echo "GIT CLONE"
-git clone https://github.com/yradsmikham/walmart-k8s.git
-cd walmart-k8s
+git clone https://github.com/$AKS_MANIFEST_REPO.git
+repo_url=https://github.com/$AKS_MANIFEST_REPO.git
+repo=${repo_url##*/}
+echo "REPO:$repo"
+repo_name=${repo%.*}
+echo "REPO_NAME:$repo_name"
+cd $repo_name
 
 echo "GIT CHECKOUT"
 git checkout master
@@ -38,11 +39,11 @@ git status
 echo "Copy yaml files to repo directory..."
 rm -rf prod/
 cp -r /home/vsts/work/1/s/generated/* .
-ls /home/vsts/work/1/s/walmart-k8s
+ls /home/vsts/work/1/s/$repo_name
 echo "GIT ADD"
 git add *
 
-#Set git identity 
+# Set git identity 
 git config user.email "admin@azuredevops.com"
 git config user.name "Automated Account"
 
@@ -53,6 +54,6 @@ git status
 echo "GIT PULL" 
 git pull
 echo "GIT PUSH"
-git push https://$ACCESS_TOKEN@github.com/yradsmikham/walmart-k8s.git
+git push https://$ACCESS_TOKEN@github.com/$AKS_MANIFEST_REPO.git
 echo "GIT STATUS"
 git status
