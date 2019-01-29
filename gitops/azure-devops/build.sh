1802c12a346fa375214201436e17ba1ee9342ee2
Samiya Akhtar <saakhta@microsoft.com>
2019-01-29 14:00:52 -0800

    Added error handling and minor bug fix, with README

--- gitops/azure-devops/build.sh
+++ gitops/azure-devops/build.sh
@@ -5,15 +5,16 @@ if [ -z "$VERSION" ]
 then
     VERSIONS=$(curl -s https://api.github.com/repos/Microsoft/fabrikate/tags)
     LATEST_RELEASE=$(echo $VERSIONS | grep "name" | head -1)
-    LATEST_VERSION=`echo "$LATEST_RELEASE" | cut -d'"' -f 4`
+    VERSION_TO_DOWNLOAD=`echo "$LATEST_RELEASE" | cut -d'"' -f 4`
 else
     echo "Fabrikate Version: $VERSION"
+    VERSION_TO_DOWNLOAD=$VERSION
 fi
 
 echo "Downloading Fabrikate..."
-echo "Latest Fabrikate Version: $LATEST_VERSION"
-wget "https://github.com/Microsoft/fabrikate/releases/download/$LATEST_VERSION/fab-v$LATEST_VERSION-linux-amd64.zip"
-unzip fab-v$LATEST_VERSION-linux-amd64.zip -d fab
+echo "Latest Fabrikate Version: $VERSION_TO_DOWNLOAD"
+wget "https://github.com/Microsoft/fabrikate/releases/download/$VERSION_TO_DOWNLOAD/fab-v$VERSION_TO_DOWNLOAD-linux-amd64.zip"
+unzip fab-v$VERSION_TO_DOWNLOAD-linux-amd64.zip -d fab
 export PATH=$PATH:/home/vsts/work/1/s/fab
 fab install
 
@@ -21,11 +22,23 @@ fab generate prod
 echo "FAB GENERATE PROD COMPLETED"
 ls -a
 
-# Git
+# If generated folder is empty, quit
+# In the case that all components are removed from the source hld, 
+# generated folder should still not be empty
+if find "/home/vsts/work/1/s/generated" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
+    echo "Manifest files have been generated"
+else
+    echo "Manifest files could not be generated, quitting"
+    exit 1
+fi
+
+# Clone the destination repo
 cd /home/vsts/work/1/s/
 echo "GIT CLONE"
 git clone https://github.com/$AKS_MANIFEST_REPO.git
 repo_url=https://github.com/$AKS_MANIFEST_REPO.git
+
+# Extract repo name from url
 repo=${repo_url##*/}
 echo "REPO:$repo"
 repo_name=${repo%.*}
@@ -48,7 +61,7 @@ git config user.email "admin@azuredevops.com"
 git config user.name "Automated Account"
 
 echo "GIT COMMIT"
-git commit -m "Updated k8s manifest files"
+git commit -m "Updated k8s manifest files post commit: $COMMIT_MESSAGE"
 echo "GIT STATUS" 
 git status
 echo "GIT PULL" 
