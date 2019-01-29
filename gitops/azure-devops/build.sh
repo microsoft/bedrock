08ba90c074c817c5c8121b691e7c7e6d6ba8258f
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2019-01-29 15:25:02 -0800

    Parameterized helm repo url

--- gitops/azure-devops/build.sh
+++ gitops/azure-devops/build.sh
@@ -14,7 +14,7 @@ fi
 echo "RUN HELM INIT"
 helm init
 echo "HELM ADD INCUBATOR"
-helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com/
+helm repo add $HELM_CHART_REPO $HELM_CHART_REPO_URL
 
 echo "Downloading Fabrikate..."
 echo "Latest Fabrikate Version: $VERSION_TO_DOWNLOAD"
