be202b829815cc82af07477c17e04b85d4571f85
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2019-01-30 14:02:14 -0800

    helm default repo and repo url will be used when helm variables are not specified (#59)

--- gitops/azure-devops/build.sh
+++ gitops/azure-devops/build.sh
@@ -14,9 +14,7 @@ fi
 echo "RUN HELM INIT"
 helm init
 echo "HELM ADD INCUBATOR"
-echo "HELM_CHART_REPO:$HELM_CHART_REPO"
-echo "HELM_CHART_REPO_URL:$HELM_CHART_REPO_URL"
-if [ -z "$HELM_CHART_REPO"] && [ -z "$HELM_CHART_REPO_URL"]
+if [ -z "$HELM_CHART_REPO"] || [ -z "$HELM_CHART_REPO_URL" ];
 then
     echo "Using DEFAULT helm repo..."
     helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com/
