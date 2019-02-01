f7047dc1e5a787bdc6c799696a67e0688822d215
Samiya Akhtar <saakhta@microsoft.com>
2019-01-31 16:34:53 -0800

    PR verification and break down of shell script

--- gitops/azure-devops/README.md
+++ gitops/azure-devops/README.md
@@ -20,11 +20,17 @@ steps:
   persistCredentials: true
   clean: true
 
+- bash: |
+    chmod +x ./build.sh && ./build.sh --verify-only
+  condition: eq(variables['Build.Reason'], 'PullRequest')
+
 - task: ShellScript@2
   inputs:
     scriptPath: build.sh
+  condition: ne(variables['Build.Reason'], 'PullRequest')
   env:
     ACCESS_TOKEN: $(accesstoken)
     COMMIT_MESSAGE: $(Build.SourceVersionMessage)
     AKS_MANIFEST_REPO: $(aks_manifest_repo)
+
 ```
--- gitops/azure-devops/build.sh
+++ gitops/azure-devops/build.sh
@@ -1,84 +1,136 @@
-cd /home/vsts/work/1/s/
+#!/bin/bash
 
+function copy_files() {
+    cp -r * $HOME/
+    cd $HOME
+}
+
+# Initialize Helm
+function helm_init() {
+    echo "RUN HELM INIT"
+    helm init
+    echo "HELM ADD INCUBATOR"
+    if [ -z "$HELM_CHART_REPO" ] || [ -z "$HELM_CHART_REPO_URL" ];
+    then
+        echo "Using DEFAULT helm repo..."
+        helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com/
+    else
+        echo "Using DEFINED helm repo..."
+        helm repo add $HELM_CHART_REPO $HELM_CHART_REPO_URL
+    fi
+}
+
+# Obtain version for Fabrikate
 # If the version number is not provided, then download the latest
-if [ -z "$VERSION" ]
-then
-    VERSIONS=$(curl -s https://api.github.com/repos/Microsoft/fabrikate/tags)
-    LATEST_RELEASE=$(echo $VERSIONS | grep "name" | head -1)
-    VERSION_TO_DOWNLOAD=`echo "$LATEST_RELEASE" | cut -d'"' -f 4`
+function get_fab_version() {
+    if [ -z "$VERSION" ]
+    then
+        VERSIONS=$(curl -s https://api.github.com/repos/Microsoft/fabrikate/tags)
+        LATEST_RELEASE=$(echo $VERSIONS | grep "name" | head -1)
+        VERSION_TO_DOWNLOAD=`echo "$LATEST_RELEASE" | cut -d'"' -f 4`
+    else
+        echo "Fabrikate Version: $VERSION"
+        VERSION_TO_DOWNLOAD=$VERSION
+    fi
+}
+
+# Download Fabrikate and install
+function download_fab() {
+    echo "DOWNLOADING FABRIKATE..."
+    echo "Latest Fabrikate Version: $VERSION_TO_DOWNLOAD"
+    wget "https://github.com/Microsoft/fabrikate/releases/download/$VERSION_TO_DOWNLOAD/fab-v$VERSION_TO_DOWNLOAD-linux-amd64.zip"
+    unzip fab-v$VERSION_TO_DOWNLOAD-linux-amd64.zip -d fab
+    ls
+    export PATH=$PATH:$HOME/fab
+    fab install
+    echo "FAB INSTALL COMPLETED"
+}
+
+# Run fab generate
+function fab_generate() {
+    fab generate prod
+    echo "FAB GENERATE COMPLETED"
+    ls -a
+
+    # If generated folder is empty, quit
+    # In the case that all components are removed from the source hld, 
+    # generated folder should still not be empty
+    if find "$HOME/generated" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
+        echo "Manifest files have been generated."
+    else
+        echo "Manifest files could not be generated, quitting..."
+        exit 1
+    fi  
+}
+
+# Authenticate with Git
+function git_connect() {
+    cd $HOME
+    echo "GIT CLONE"
+    git clone https://github.com/$AKS_MANIFEST_REPO.git
+    repo_url=https://github.com/$AKS_MANIFEST_REPO.git
+    repo=${repo_url##*/}
+
+    # Extract repo name from url
+    repo_name=${repo%.*}
+    cd $repo_name
+}
+
+# Git commit
+function git_commit() {
+    echo "GIT CHECKOUT"
+    git checkout master
+    echo "GIT STATUS"
+    git status
+    echo "COPY YAML FILES TO REPO DIRECTORY..."
+    rm -rf prod/
+    cp -r $HOME/generated/* .
+    ls $HOME/$repo_name
+    echo "GIT ADD"
+    git add *
+
+    #Set git identity 
+    git config user.email "admin@azuredevops.com"
+    git config user.name "Automated Account"
+
+    echo "GIT COMMIT"
+    git commit -m "Updated k8s manifest files post commit: $COMMIT_MESSAGE"
+    echo "GIT STATUS" 
+    git status
+    echo "GIT PULL" 
+    git pull
+}
+
+# Perform a Git push
+function git_push() {
+    echo "GIT PUSH"
+    git push https://$ACCESS_TOKEN@github.com/$AKS_MANIFEST_REPO.git
+    echo "GIT STATUS"
+    git status
+}
+
+function verify() {
+    echo "Starting verification"
+    copy_files
+    helm_init
+    get_fab_version
+    download_fab
+    fab_generate
+}
+
+# Run functions
+function verify_and_push() {
+    verify
+    echo "Verification complete, push to yaml repo"
+    git_connect
+    git_commit
+    git_push
+}
+
+if [ "${1}" == "--verify-only" ]; then
+    verify
+elif [ "${1}" != "--source-only" ]; then
+    verify_and_push "${@}"
 else
-    echo "Fabrikate Version: $VERSION"
-    VERSION_TO_DOWNLOAD=$VERSION
+    verify_and_push
 fi
-
-echo "RUN HELM INIT"
-helm init
-echo "HELM ADD INCUBATOR"
-if [ -z "$HELM_CHART_REPO"] || [ -z "$HELM_CHART_REPO_URL" ];
-then
-    echo "Using DEFAULT helm repo..."
-    helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com/
-else
-    echo "Using DEFINED help repo..."
-    helm repo add $HELM_CHART_REPO $HELM_CHART_REPO_URL
-fi
-
-echo "Downloading Fabrikate..."
-echo "Latest Fabrikate Version: $VERSION_TO_DOWNLOAD"
-wget "https://github.com/Microsoft/fabrikate/releases/download/$VERSION_TO_DOWNLOAD/fab-v$VERSION_TO_DOWNLOAD-linux-amd64.zip"
-unzip fab-v$VERSION_TO_DOWNLOAD-linux-amd64.zip -d fab
-export PATH=$PATH:/home/vsts/work/1/s/fab
-fab install
-
-fab generate prod
-echo "FAB GENERATE PROD COMPLETED"
-ls -a
-
-# If generated folder is empty, quit
-# In the case that all components are removed from the source hld, 
-# generated folder should still not be empty
-if find "/home/vsts/work/1/s/generated" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then
-    echo "Manifest files have been generated"
-else
-    echo "Manifest files could not be generated, quitting"
-    exit 1
-fi
-
-# Clone the destination repo
-cd /home/vsts/work/1/s/
-echo "GIT CLONE"
-git clone https://github.com/$AKS_MANIFEST_REPO.git
-repo_url=https://github.com/$AKS_MANIFEST_REPO.git
-
-# Extract repo name from url
-repo=${repo_url##*/}
-echo "REPO:$repo"
-repo_name=${repo%.*}
-echo "REPO_NAME:$repo_name"
-cd $repo_name
-
-echo "GIT CHECKOUT"
-git checkout master
-echo "GIT STATUS"
-git status
-echo "Copy yaml files to repo directory..."
-rm -rf prod/
-cp -r /home/vsts/work/1/s/generated/* .
-ls /home/vsts/work/1/s/$repo_name
-echo "GIT ADD"
-git add *
-
-# Set git identity 
-git config user.email "admin@azuredevops.com"
-git config user.name "Automated Account"
-
-echo "GIT COMMIT"
-git commit -m "Updated k8s manifest files post commit: $COMMIT_MESSAGE"
-echo "GIT STATUS" 
-git status
-echo "GIT PULL" 
-git pull
-echo "GIT PUSH"
-git push https://$ACCESS_TOKEN@github.com/$AKS_MANIFEST_REPO.git
-echo "GIT STATUS"
-git status
