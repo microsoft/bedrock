4d6dafea3c8932f7a3c0b484d6bbacb2bc16acf5
David Tesar <david.tesar@microsoft.com>
2019-02-11 18:17:50 -0800

    mv kubeconfig, make sensitive output

--- cluster/azure/aks/main.tf
+++ cluster/azure/aks/main.tf
@@ -46,13 +46,3 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     client_secret = "${var.service_principal_secret}"
   }
 }
-
-resource "null_resource" "cluster_credentials" {
-  count  = "${var.enable_cluster_creds_to_disk ? 1 : 0}"
-  provisioner "local-exec" {
-    command = "if [ ! -e ${var.output_directory} ]; then mkdir -p ${var.output_directory}; fi && echo \"${azurerm_kubernetes_cluster.cluster.kube_config}\" > ${var.output_directory}/kube_config"
-  }
-
-  depends_on = ["azurerm_kubernetes_cluster.cluster"]
-
-}
