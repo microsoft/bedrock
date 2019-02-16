c6778b80d8aa20b81d191781a376be0d500674f8
Sarath Pinninty <sarathp@microsoft.com>
2019-02-15 17:36:40 -0800

    Added traffic manager monior config variables in 'multiple clusters' environment.

--- cluster/environments/azure-multiple-clusters/trafficmanager-variables.tf
+++ cluster/environments/azure-multiple-clusters/trafficmanager-variables.tf
@@ -13,3 +13,13 @@ variable "traffic_manager_profile_name" {
 variable "traffic_manager_dns_name" {
   type = "string"
 }
+
+variable "traffic_manager_monitor_protocol" {
+  type    = "string"
+  default = "http"
+}
+
+variable "traffic_manager_monitor_port" {
+  type    = "string"
+  default = "80"
+}
--- cluster/environments/azure-multiple-clusters/trafficmanager.tf
+++ cluster/environments/azure-multiple-clusters/trafficmanager.tf
@@ -6,10 +6,12 @@ resource "azurerm_resource_group" "tmrg" {
 module "trafficmanager" {
   source = "../../azure/tm-profile"
 
-  resource_group_name          = "${azurerm_resource_group.tmrg.name}"
-  resource_group_location      = "${azurerm_resource_group.tmrg.location}"
-  traffic_manager_profile_name = "${var.traffic_manager_profile_name}"
-  traffic_manager_dns_name     = "${var.traffic_manager_dns_name}"
+  resource_group_name              = "${azurerm_resource_group.tmrg.name}"
+  resource_group_location          = "${azurerm_resource_group.tmrg.location}"
+  traffic_manager_profile_name     = "${var.traffic_manager_profile_name}"
+  traffic_manager_dns_name         = "${var.traffic_manager_dns_name}"
+  traffic_manager_monitor_protocol = "${var.traffic_manager_monitor_protocol}"
+  traffic_manager_monitor_port     = "${var.traffic_manager_monitor_port}"
 
   tags = {
     environment = "azure-multiple-clusters"
