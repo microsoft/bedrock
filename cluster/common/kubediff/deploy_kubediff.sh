d86f380ba7bfa972124a57ebd686dbc7cd97f0e2
Samiya Akhtar <saakhta@microsoft.com>
2019-02-20 09:10:42 -0800

    Fixed regex sed command

--- cluster/common/kubediff/deploy_kubediff.sh
+++ cluster/common/kubediff/deploy_kubediff.sh
@@ -19,10 +19,17 @@ fi
 echo "Cloned kubediff. cd"
 cd $REPO_DIR/k8s
 
-if ! sed -i -e 's/<your config repo>/$GITOPS_URL/g' ./kubediff-rc.yaml; then
-    echo "ERROR: failed to update with gitops url $GITOPS_URL"
-    exit 1
+re="^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/(.+).git$"
+if [[ $GITOPS_URL =~ $re ]]; then
+    user=${BASH_REMATCH[4]}
+    repo=${BASH_REMATCH[5]}
+
+    if ! sed -i -e "s|<your config repo>|$user/$repo|g" ./kubediff-rc.yaml; then
+        echo "ERROR: failed to update with gitops url $GITOPS_URL"
+        exit 1
+    fi
 fi
+
 sed '23q;d' ./kubediff-rc.yaml
 echo "Updated with gitops url $GITOPS_URL"
 
