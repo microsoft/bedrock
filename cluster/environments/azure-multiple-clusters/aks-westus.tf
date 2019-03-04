0a080fa259b208a7cf8afdfd995b30aaf0f20b9a
Sean Usher <43831561+seushermsft@users.noreply.github.com>
2019-03-04 13:33:36 -0700

    Support flux recreate with existing install (#168)

--- cluster/environments/azure-multiple-clusters/aks-westus.tf
+++ cluster/environments/azure-multiple-clusters/aks-westus.tf
@@ -49,7 +49,7 @@ module "west_flux" {
 
   gitops_ssh_url      = "${var.gitops_ssh_url}"
   gitops_ssh_key      = "${var.gitops_ssh_key}"
-  flux_recreate       = ""
+  flux_recreate       = "${var.flux_recreate}"
   kubeconfig_complete = "${module.west_aks.kubeconfig_done}"
   kubeconfig_filename = "${local.west_kubeconfig_filename}"
   flux_clone_dir      = "${local.west_flux_clone_dir}"
