edaa30fb03645c20614374f43981517b8eda956d
Tim Park <timfpark@gmail.com>
2019-03-25 13:34:12 -0700

    remove vnet outputs

--- cluster/azure/aks-gitops/outputs.tf
+++ cluster/azure/aks-gitops/outputs.tf
@@ -1,11 +1,3 @@
 output "kubeconfig_done" {
   value = "${module.aks.kubeconfig_done}"
 }
-
-output "vnet_id" {
-  value = "${module.vnet.vnet_id}"
-}
-
-output "vnet_name" {
-  value = "${module.vnet.vnet_name}"
-}
