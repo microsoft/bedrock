3db9d4c19cf97f05ca19fdc2c1891158d0048283
ReenuSaluja <reenu_grewal@rediffmail.com>
2019-04-09 10:49:11 +0530

    updated api-mgmt terraform code

---
+++ cluster/environments/azure-multiple-clusters-waf-tm-apimgt/aks-westus-variables.tf
@@ -0,0 +1,41 @@
+variable "west_resource_group_name" {
+  type = "string"
+}
+
+variable "west_resource_group_location" {
+  type    = "string"
+}
+
+variable "gitops_west_path" {
+  type = "string"
+}
+<<<<<<< HEAD
+=======
+
+variable "west_address_space" {
+  description = "The address space that is used by the virtual network."
+  default     = "172.24.0.0/16"
+}
+
+variable "west_subnet_prefixes" {
+  description = "The address prefix to use for the subnet."
+  default     = ["172.24.0.0/20"]
+}
+
+variable "west_service_cidr" {
+  default = "172.25.0.0/16"
+  description ="Used to assign internal services in the AKS cluster an IP address. This IP address range should be an address space that isn't in use elsewhere in your network environment. This includes any on-premises network ranges if you connect, or plan to connect, your Azure virtual networks using Express Route or a Site-to-Site VPN connections."
+  type = "string"
+}
+
+variable "west_dns_ip" {
+  default = "172.25.0.10"
+  description = "should be the .10 address of your service IP address range"
+  type = "string"
+}
+
+variable "west_docker_cidr" {
+  default = "172.19.0.1/16"
+  description = "IP address (in CIDR notation) used as the Docker bridge IP address on nodes. Default of 172.17.0.1/16."
+}
+>>>>>>> 97590d70e78cb66420a10bcb742868c4a878f946
---
+++ cluster/environments/azure-multiple-clusters-waf-tm-apimgt/api-management-variables.tf
@@ -0,0 +1,15 @@
+
+variable "traffic_manager_url" {
+    default = ""
+}
+variable "api_management_resource_group_name" {
+    default = "apimgmtresgrp5" 
+}
+
+variable "service_option1apim_name" {
+  default = ""
+}
+variable "location" {
+  description = "The location/region where the api management service will be deployed. The full list of Azure regions can be found at https://azure.microsoft.com/regions"
+  type = "string"
+}
