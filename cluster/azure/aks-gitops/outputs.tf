116561a51b47812c920dd58cca608767650094ee
Michael Tarng <20913254+mtarng@users.noreply.github.com>
2019-04-29 12:39:36 -0700

    removing unused outputs from aks and aks-gitops. Updating azure-multiple-clusters README

--- cluster/azure/aks-gitops/outputs.tf
+++ cluster/azure/aks-gitops/outputs.tf
@@ -5,7 +5,3 @@ output "kubeconfig_done" {
 output "aks_flux_kubediff_done" {
   value = "${module.aks.kubeconfig_done}_${module.flux.flux_done}_${module.kubediff.kubediff_done}"
 }
-
-output "cluster_derived_resource_group" {
-  value = "${module.aks.cluster_derived_resource_group}"
-}
--- cluster/azure/aks/outputs.tf
+++ cluster/azure/aks/outputs.tf
@@ -11,7 +11,3 @@ output "kube_config" {
 output "kubeconfig_done" {
   value = "${join("",null_resource.cluster_credentials.*.id)}"
 }
-
-output "cluster_derived_resource_group" {
-  value = "${azurerm_kubernetes_cluster.cluster.node_resource_group}"
-}
