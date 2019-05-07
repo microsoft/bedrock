fb2cf2f40f92167da3a14b0059dcaad8755bfc57
ReenuSaluja <reenu_grewal@rediffmail.com>
2019-05-07 14:30:04 +0530

    Update main.tf

--- cluster/environments/azure-multiple-clusters-waf-tm-apimgmt/main.tf
+++ cluster/environments/azure-multiple-clusters-waf-tm-apimgmt/main.tf
@@ -4,6 +4,5 @@ module "provider" {
 
 # Read AKS cluster service principal (client) object to create a role assignment
 data "azuread_service_principal" "sp" {
-  //application_id = "${var.service_principal_id}"
-  application_id = "f8ec33d6-dd15-45ef-a8f0-fb9d25c8f835"
+  application_id = "${var.service_principal_id}"
 }
