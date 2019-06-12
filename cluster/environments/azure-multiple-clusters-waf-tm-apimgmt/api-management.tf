df500e3253cf2771651c97c2a25caec3ee4861f6
ReenuSaluja <reenu_grewal@rediffmail.com>
2019-06-12 12:26:31 +0530

    Update api-management.tf

--- cluster/environments/azure-multiple-clusters-waf-tm-apimgmt/api-management.tf
+++ cluster/environments/azure-multiple-clusters-waf-tm-apimgmt/api-management.tf
@@ -5,8 +5,4 @@ module "api-mgmt" {
   resource_group_name = "${azurerm_resource_group.tmrg.name}"
   traffic_manager_fqdn = "${module.trafficmanager.traffic_manager_fqdn}"
   service_apim_name = "${var.service_apim_name}"
-
-  tags = {
-    environment = "azure-multiple-clusters-waf-tm-apimgmt"
-  }
 }
