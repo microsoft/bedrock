43e570dab765db292dac3471deb8fc92fc4956cc
ReenuSaluja <reenu_grewal@rediffmail.com>
2019-06-12 17:53:51 +0530

    Update trafficmanager-variables.tf

--- cluster/environments/azure-multiple-clusters-waf-tm-apimgmt/trafficmanager-variables.tf
+++ cluster/environments/azure-multiple-clusters-waf-tm-apimgmt/trafficmanager-variables.tf
@@ -22,8 +22,8 @@ variable "traffic_manager_dns_name" {
 variable "traffic_manager_monitor_protocol" {
   type    = "string"
   default = "http"
-}
-
+} 
+ 
 variable "traffic_manager_monitor_port" {
   type    = "string"
   default = "80"
