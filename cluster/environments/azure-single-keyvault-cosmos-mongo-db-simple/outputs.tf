d54585a54d7614c509675c5b5e0409a6a01d888d
Michael Tarng <20913254+mtarng@users.noreply.github.com>
2019-07-09 14:21:30 -0700

    terraform linting

--- cluster/environments/azure-single-keyvault-cosmos-mongo-db-simple/outputs.tf
+++ cluster/environments/azure-single-keyvault-cosmos-mongo-db-simple/outputs.tf
@@ -13,41 +13,49 @@
 # module "cosmos_mongo_db"
 
 output "azure_cosmos_db_endpoint" {
-    value = "${module.cosmos_mongo_db.cosmos_db_endpoint}"
+  value = "${module.cosmos_mongo_db.cosmos_db_endpoint}"
 }
 
 output "azure_cosmos_db_primary_master_key" {
-    value = "${module.cosmos_mongo_db.cosmos_db_primary_master_key}"
+  value = "${module.cosmos_mongo_db.cosmos_db_primary_master_key}"
 }
 
 # output "cosmos_db_id" {
 #   value = "${azurerm_cosmosdb_account.cosmosdb_account.id}"
 # }
 
+
 # output "cosmos_db_endpoint" {
 #   value = "${azurerm_cosmosdb_account.cosmosdb_account.endpoint}"
 # }
+
 
 # output "cosmos_db_endpoints_read" {
 #   value = "${azurerm_cosmosdb_account.cosmosdb_account.read_endpoints}"
 # }
 
+
 # output "cosmos_db_endpoints_write" {
 #   value = "${azurerm_cosmosdb_account.cosmosdb_account.write_endpoints}"
 # }
+
 
 # output "cosmos_db_primary_master_key" {
 #   value = "${azurerm_cosmosdb_account.cosmosdb_account.primary_master_key}"
 # }
 
+
 # output "cosmos_db_secondary_master_key" {
 #   value = "${azurerm_cosmosdb_account.cosmosdb_account.secondary_master_key}"
 # }
+
 
 # output "cosmos_db_primary_readonly_master_key" {
 #   value = "${azurerm_cosmosdb_account.cosmosdb_account.primary_readonly_master_key}"
 # }
 
+
 # output "cosmos_db_secondary_readonly_master_key" {
 #   value = "${azurerm_cosmosdb_account.cosmosdb_account.secondary_readonly_master_key}"
 # }
+
