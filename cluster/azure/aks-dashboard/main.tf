7b7cce0ba32b1e995e175f69be7c8ea822afe1e6
xiaodoli <xiaodoli@microsoft.com>
2019-08-07 16:22:59 -0700

    remove resource dep

--- cluster/azure/aks-dashboard/main.tf
+++ cluster/azure/aks-dashboard/main.tf
@@ -4,6 +4,4 @@ resource "null_resource" "dashboard_access" {
   provisioner "local-exec" {
     command = "echo 'Need to use this var so terraform waits for kubeconfig ' ${var.kubeconfigadmin_done};KUBECONFIG=${var.output_directory}/${var.kubeconfigadmin_filename} if [ ${var.dashboard_cluster_role} = 'cluster_admin' ]; then kubectl apply -f ${path.module}/dashboard_admin_rbac.yaml else kubectl apply -f ${path.module}/dashboard_rbac.yaml fi"
   }
-
-  depends_on = ["${module.aks.azurerm_kubernetes_cluster.cluster}"]
 }
