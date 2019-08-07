c390a5149bb3cafcf51e5399fb347986a2b2efbf
xiaodoli <xiaodoli@microsoft.com>
2019-08-07 16:12:37 -0700

    dev space

--- cluster/azure/aks-dashboard/main.tf
+++ cluster/azure/aks-dashboard/main.tf
@@ -5,9 +5,5 @@ resource "null_resource" "dashboard_access" {
     command = "echo 'Need to use this var so terraform waits for kubeconfig ' ${var.kubeconfigadmin_done};KUBECONFIG=${var.output_directory}/${var.kubeconfigadmin_filename} if [ ${var.dashboard_cluster_role} = 'cluster_admin' ]; then kubectl apply -f ${path.module}/dashboard_admin_rbac.yaml else kubectl apply -f ${path.module}/dashboard_rbac.yaml fi"
   }
 
-  triggers = {
-    enable_azure_monitoring  = "${var.enable_azure_monitoring}"
-  }
-
   depends_on = ["${module.aks.azurerm_kubernetes_cluster.cluster}"]
 }
