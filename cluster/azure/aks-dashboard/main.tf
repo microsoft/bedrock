1f6204c1fcedb3f0e86f11a5d9d218eabc25e751
xiaodoli <xiaodoli@microsoft.com>
2019-08-07 19:21:59 -0700

    fix path var

--- cluster/azure/aks-dashboard/main.tf
+++ cluster/azure/aks-dashboard/main.tf
@@ -2,6 +2,6 @@ resource "null_resource" "dashboard_access" {
   count = "${var.dashboard_cluster_role != "" ? 1 : 0}"
 
   provisioner "local-exec" {
-    command = "echo 'Need to use this var so terraform waits for kubeconfig ' ${var.kubeconfigadmin_done};KUBECONFIG=${var.output_directory}/${var.kubeconfigadmin_filename} ${path.module}/apply_dashboard_rbac.sh -r ${var.dashboard_cluster_role} -a {path.module}/dashboard_admin_rbac.yaml -u {path.module}/dashboard_rbac.yaml"
+    command = "echo 'Need to use this var so terraform waits for kubeconfig ' ${var.kubeconfigadmin_done};KUBECONFIG=${var.output_directory}/${var.kubeconfigadmin_filename} ${path.module}/apply_dashboard_rbac.sh -r ${var.dashboard_cluster_role} -a ${path.module}/dashboard_admin_rbac.yaml -u ${path.module}/dashboard_rbac.yaml"
   }
 }
