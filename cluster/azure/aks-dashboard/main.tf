5c6593d471d7b177e3b3677dc914b22e3bf1772a
xiaodoli <xiaodoli@microsoft.com>
2019-08-07 18:40:51 -0700

    fix error

--- cluster/azure/aks-dashboard/main.tf
+++ cluster/azure/aks-dashboard/main.tf
@@ -2,6 +2,6 @@ resource "null_resource" "dashboard_access" {
   count = "${var.dashboard_cluster_role != "" ? 1 : 0}"
 
   provisioner "local-exec" {
-    command = "echo 'Need to use this var so terraform waits for kubeconfig ' ${var.kubeconfigadmin_done};KUBECONFIG=${var.output_directory}/${var.kubeconfigadmin_filename} if [ ${var.dashboard_cluster_role} = 'cluster_admin' ]; then kubectl apply -f ${path.module}/dashboard_admin_rbac.yaml else kubectl apply -f ${path.module}/dashboard_rbac.yaml fi"
+    command = "echo 'Need to use this var so terraform waits for kubeconfig ' ${var.kubeconfigadmin_done};KUBECONFIG=${var.output_directory}/${var.kubeconfigadmin_filename} if [ ${var.dashboard_cluster_role} = 'cluster_admin' ]; then kubectl apply -f '${path.module}/dashboard_admin_rbac.yaml'; else kubectl apply -f '${path.module}/dashboard_rbac.yaml'; fi"
   }
 }
