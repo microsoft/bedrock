57771c954962083adfd6740f230b35c3df152df0
lingxd <lingxd@gmail.com>
2019-08-09 04:30:28 -0700

    generate owner yaml

--- cluster/azure/aks-rbac/apply_aad_rbac.sh
+++ cluster/azure/aks-rbac/apply_aad_rbac.sh
@@ -11,24 +11,28 @@ do
  esac
 done
 
-if [ ${#OWNERS[@]} > 0 ]; then
-    $OWNER_YAML_FILE = "$OUTPUT_FOLDER/owners.yaml"
-    "apiVersion: rbac.authorization.k8s.io/v1" >> $OWNER_YAML_FILE
-    "kind: ClusterRoleBinding" >> $OWNER_YAML_FILE
-    "metadata:" >> $OWNER_YAML_FILE
-    "  name: aks-cluster-admins" >> $OWNER_YAML_FILE
-    "roleRef:" >> $OWNER_YAML_FILE
-    "  apiGroup: rbac.authorization.k8s.io" >> $OWNER_YAML_FILE
-    "  kind: ClusterRole" >> $OWNER_YAML_FILE
-    "  name: cluster-admin" >> $OWNER_YAML_FILE
-    "subjects:" >> $OWNER_YAML_FILE
-    `
+if [ ${#OWNERS[@]} -eq 0 ]; then
+    echo "OWNERS is empty"
+else
+    OWNER_YAML_FILE="$OUTPUT_FOLDER/owners.yaml"
+    echo "Generating yaml file for owners: $OWNER_YAML_FILE"
+    rm -f $OWNER_YAML_FILE
+    echo "apiVersion: rbac.authorization.k8s.io/v1" >> $OWNER_YAML_FILE
+    echo "kind: ClusterRoleBinding" >> $OWNER_YAML_FILE
+    echo "metadata:" >> $OWNER_YAML_FILE
+    echo "  name: aks-cluster-admins" >> $OWNER_YAML_FILE
+    echo "roleRef:" >> $OWNER_YAML_FILE
+    echo "  apiGroup: rbac.authorization.k8s.io" >> $OWNER_YAML_FILE
+    echo "  kind: ClusterRole" >> $OWNER_YAML_FILE
+    echo "  name: cluster-admin" >> $OWNER_YAML_FILE
+    echo "subjects:" >> $OWNER_YAML_FILE
+
     for i in "${OWNERS[@]}"
     do
-        "  - apiGroup: rbac.authorization.k8s.io" >> $OWNER_YAML_FILE
-        "    kind: User" >> $OWNER_YAML_FILE
-        "    name: $i" >> $OWNER_YAML_FILE
+        echo "  - apiGroup: rbac.authorization.k8s.io" >> $OWNER_YAML_FILE
+        echo "    kind: User" >> $OWNER_YAML_FILE
+        echo "    name: $i" >> $OWNER_YAML_FILE
     done
-    ;
-    kubectl apply -f $OWNER_YAML_FILE
+
+    kubectl apply -f $OWNER_YAML_FILE;
 fi
