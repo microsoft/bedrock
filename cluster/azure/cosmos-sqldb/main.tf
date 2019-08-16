d15b4ddcd2e1b1dd41ab3687e1c989f588ccfdb4
lingxd <lingxd@gmail.com>
2019-08-16 01:49:17 -0700

    threshold

--- cluster/azure/cosmos-sqldb/main.tf
+++ cluster/azure/cosmos-sqldb/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_cosmosdb_account" "cosmosdb_account" {
 
   consistency_policy {
     consistency_level       = "${var.consistency_level}"
-    max_interval_in_seconds = 10
+    max_interval_in_seconds = 330
     max_staleness_prefix    = 100010
   }
 
