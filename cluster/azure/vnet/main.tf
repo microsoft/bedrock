5e985027a58cde2b9220f6140dd3234d39f50023
xiaodoli <xiaodoli@microsoft.com>
2019-08-16 15:09:23 -0700

    fix subnet

--- cluster/azure/vnet/main.tf
+++ cluster/azure/vnet/main.tf
@@ -13,11 +13,8 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  count                = "${length(var.subnet_names)}"
-  name                 = "${var.subnet_names[count.index]}"
+  name                 = "subnet"
   virtual_network_name = "${azurerm_virtual_network.vnet.name}"
   resource_group_name  = "${azurerm_resource_group.vnet.name}"
-
-  address_prefix    = "${var.subnet_prefixes[count.index]}"
-  service_endpoints = "${var.subnet_service_endpoints[count.index]}"
+  address_prefix       = "10.0.1.0/24"
 }
