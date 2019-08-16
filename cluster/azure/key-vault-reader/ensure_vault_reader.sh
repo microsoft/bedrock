2fb5112ff8f3e2b919c9c62b0e8c6067be3a6cd0
xiaodoli <xiaodoli@microsoft.com>
2019-08-16 12:24:24 -0700

    use quote: sh interpolation

--- cluster/azure/key-vault-reader/ensure_vault_reader.sh
+++ cluster/azure/key-vault-reader/ensure_vault_reader.sh
@@ -8,6 +8,7 @@ do
     g) VAULT_RESOURCE_GROUP_NAME=${OPTARG};;
     a) AKS_CLUSTER_RESOURCE_GROUP=${OPTARG};;
     c) AKS_CLUSTER_NAME=${OPTARG};;
+    s) AKS_CLUSTER_SPN_NAME=${OPTARG};;
     l) AKS_CLUSTER_LOCATION=${OPTARG};;
     *) echo "ERROR: Please refer to usage guide on GitHub" >&2
         exit 1 ;;
@@ -27,58 +28,49 @@ if [ -z "$VAULT_RESOURCE_GROUP_NAME" ]; then
     exit 1
 fi
 
-# AZ_ACCOUNT_INFO=$(az account show)
-# AZ_CLI_SUBSCRIPTION=$(echo "$AZ_ACCOUNT_INFO" | jq -r '.id')
-
 KEY_VAULT=$(az keyvault show -g "$VAULT_RESOURCE_GROUP_NAME" -n "$VAULT_NAME")
 AZ_KEY_VAULT_ID=$(echo "$KEY_VAULT" | jq -r '.id')
+echo "Key vault id: $AZ_KEY_VAULT_ID"
 
-# echo "Check if service principal "$SERVICE_PRINCIPAL_NAME" exist"
-# EXISTING_SPNS=$(az ad sp list --display-name "$SERVICE_PRINCIPAL_NAME")
-# EXISTING_SPN_FOUND=$(echo "$EXISTING_SPNS" | jq '. | length')
+AKS_RESOURCE_GROUP_ID=$(az group show -n "$AKS_CLUSTER_RESOURCE_GROUP" | jq '.id')
+echo "AKS resource group id: $AKS_RESOURCE_GROUP_ID"
 
-# if [ $EXISTING_SPN_FOUND -eq 0 ]; then
-#     echo "Creating new service principal: $SERVICE_PRINCIPAL_NAME"
-#     SERVICE_PRINCIPAL=$(az ad sp create-for-rbac --name "$SERVICE_PRINCIPAL_NAME" --role "Reader" --scopes "$AZ_KEY_VAULT_ID")
-#     echo "Service principal:"
-#     echo "$SERVICE_PRINCIPAL"
-# else
-#     SPN_APP_ID=$(echo "$EXISTING_SPNS" | jq '[0].appId')
-#     EXISTING_ASSIGNMENTS=$(az role assignment list --assignee $SPN_APP_ID --role Reader --scope "$AZ_KEY_VAULT_ID")
-#     EXISTING_ASSIGNMENT_FOUND=$(echo "$EXISTING_ASSIGNMENTS" | jq '. | length')
-#     if [ $EXISTING_ASSIGNMENT_FOUND -eq 0 ]; then
-#         echo "Grant key vault reader role to service principal"
-#         NEW_ASSIGNMENT=$(az role assignment create --assignee $SPN_APP_ID --role Reader --scope "$AZ_KEY_VAULT_ID")
-#     else
-#         echo "Key vault reader role to service principal is already granted"
-#     fi
-# fi
-
-echo "Ensure msi $IDENTITY_NAME is created"
-EXISTING_IDENTTIIES=$(az identity list --resource-group $AKS_CLUSTER_RESOURCE_GROUP --query "[?name=='$IDENTITY_NAME']" -o json)
-EXISTING_IDENTITY_FOUND=$(echo "$EXISTING_IDENTTIIES" | jq '. | length')
-MSI_ID=""
-if [ $EXISTING_IDENTITY_FOUND -eq 0 ]; then
-    $MSI_CREATED=$(az identity create -g $AKS_CLUSTER_RESOURCE_GROUP -n $IDENTITY_NAME)
-    echo "Service identity:"
-    echo "$MSI_CREATED"
-    MSI_ID=$(echo "$MSI_CREATED" | jq '.clientId')
-else
-    MSI_ID=$(echo "$EXISTING_IDENTTIIES" | jq '.[0].clientId')
-fi
-
-echo "Ensure appropriate permissions are granted to msi"
 UNDERSCORE="_"
 AKS_NODE_RESOURCE_GROUP="MC$UNDERSCORE$AKS_CLUSTER_RESOURCE_GROUP$UNDERSCORE$AKS_CLUSTER_NAME$UNDERSCORE$AKS_CLUSTER_LOCATION"
-AKS_NODE_RESOURCE_GROUP_ID=$(az group show -n $AKS_NODE_RESOURCE_GROUP | jq '.id')
-az role assignment create --role "Reader" --assignee $MSI_ID --scope $AKS_NODE_RESOURCE_GROUP_ID
+AKS_NODE_RESOURCE_GROUP_ID=$(az group show -n "$AKS_NODE_RESOURCE_GROUP" | jq '.id')
+echo "MC resource group id: $AKS_NODE_RESOURCE_GROUP_ID"
 
-AKS_RESOURCE_GROUP_ID==$(az group show -n $AKS_CLUSTER_RESOURCE_GROUP | jq '.id')
-az role assignment create --role "Reader" --assignee $MSI_ID --scope $AKS_RESOURCE_GROUP_ID
+EXISTING_AKS_SPNS=$(az ad sp list --display-name "$AKS_CLUSTER_SPN_NAME")
+AKS_SPN_APP_ID=$(echo "$EXISTING_AKS_SPNS" | jq '.[0].appId')
+echo "AKS cluster spn app id: $AKS_SPN_APP_ID"
 
-az role assignment create --role "Reader" --assignee $MSI_ID --scope $AZ_KEY_VAULT_ID
+echo "Ensure msi $IDENTITY_NAME is created"
+EXISTING_IDENTTIIES="$(az identity list --resource-group "$AKS_NODE_RESOURCE_GROUP" --query "[?name=='$IDENTITY_NAME']" -o json)"
+EXISTING_IDENTITY_FOUND=$(echo "$EXISTING_IDENTTIIES" | jq '. | length')
+MSI_CLIENT_ID=""
+MSI_ID=""
+if [ $EXISTING_IDENTITY_FOUND -eq 0 ]; then
+    MSI_CREATED="$(az identity create -g "$AKS_NODE_RESOURCE_GROUP" -n "$IDENTITY_NAME" -o json)"
+    echo "Service identity:"
+    echo "$MSI_CREATED"
+    MSI_CLIENT_ID=$(echo "$MSI_CREATED" | jq '.clientId')
+    MSI_ID=$(echo "$MSI_CREATED" | jq '.id')
+else
+    MSI_CLIENT_ID=$(echo "$EXISTING_IDENTTIIES" | jq '.[0].clientId')
+fi
+echo "User-assigned identity client id: $MSI_CLIENT_ID"
+echo "User-assigned identity id: $MSI_ID"
+
+echo "Ensure appropriate permissions are granted to msi"
+echo "az role assignment create --role \"Reader\" --assignee \"$MSI_CLIENT_ID\" --scope \"$AKS_NODE_RESOURCE_GROUP_ID\""
+az role assignment create --role "Reader" --assignee "$MSI_CLIENT_ID" --scope "$AKS_NODE_RESOURCE_GROUP_ID"
+
+echo "az role assignment create --role \"Reader\" --assignee \"$MSI_CLIENT_ID\" --scope \"$AKS_RESOURCE_GROUP_ID\""
+az role assignment create --role "Reader" --assignee "$MSI_CLIENT_ID" --scope "$AKS_RESOURCE_GROUP_ID"
+
+echo "az role assignment create --role \"Reader\" --assignee \"$MSI_CLIENT_ID\" --scope \"$AZ_KEY_VAULT_ID\""
+az role assignment create --role "Reader" --assignee "$MSI_CLIENT_ID" --scope "$AZ_KEY_VAULT_ID"
 
 echo "Ensure Managed Identity Operator role is granted to aks spn"
-EXISTING_AKS_SPNS=$(az ad sp list --display-name "$AKS_CLUSTER_NAME")
-AKS_SPN_APP_ID=$(echo "$EXISTING_AKS_SPNS" | jq '.[0].appId')
-az role assignment create --role "Managed Identity Operator" --assignee $AKS_SPN_APP_ID --scope $MSI_ID
+echo "az role assignment create --role \"Managed Identity Operator\" --assignee \"$AKS_SPN_APP_ID\" --scope \"$MSI_ID\""
+az role assignment create --role "Managed Identity Operator" --assignee "$AKS_SPN_APP_ID" --scope "$MSI_ID"
---
+++ cluster/azure/key-vault-reader/readme.md
@@ -0,0 +1,86 @@
+# Setup AAD Pod Identity
+
+We will create a single pod identity and share it to all the services who need to access key vault (read-only)
+
+## Steps
+
+1. create a new identity (user-assigned) in MC resource group
+2. grant identity `Reader` role to the following scopes:
+    - MC resource group
+    - Key vault
+    - AKS cluster resource group
+
+``` bash
+az role assignment create --role Reader --assignee $identity.principalId --scope $scopeId
+```
+3. grant aks cluster spn `Managed Identity Operator` role to identity
+``` bash
+az role assignment create --role `Managed Identity Operator` --assignee $aksSpn.appId --scope $identity.id
+```
+4. for each service who is using pod identity, create `AzureIdentity`
+``` yaml
+apiVersion: "aadpodidentity.k8s.io/v1"
+kind: AzureIdentity
+metadata:
+  name: {{.Values.service.name}}
+spec:
+  type: 0
+  ResourceID: {{.Values.serviceIdentity.id}}
+  ClientId: {{.Values.serviceIdentity.clientId}}
+```
+5. for each service who is using pod identity, create `AzureIdentityBinding`
+``` yaml
+apiVersion: "aadpodidentity.k8s.io/v1"
+kind: AzureIdentityBinding
+metadata:
+  name: "{{.Values.service.name}}-id-binding"
+spec:
+  AzureIdentity: "{{.Values.service.name}}"
+  Selector: "{{.Values.service.label}}"
+```
+6. for each service who is using pod identity, add `aadpodidbinding` to labels
+    - api/web
+``` yaml
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: "{{.Values.service.name}}"
+  labels:
+    app: "{{.Values.service.name}}"
+    aadpodidbinding: "{{.Values.service.name}}"
+spec:
+  replicas: {{.Values.service.replicas}}
+  selector:
+    matchLabels:
+      app: "{{.Values.service.name}}"
+  template:
+    metadata:
+      labels:
+        app: "{{.Values.service.name}}"
+        aadpodidbinding: "{{.Values.service.name}}"
+```
+
+    - job
+``` yaml
+apiVersion: batch/v1beta1
+kind: CronJob
+metadata:
+  name: "{{.Values.service.name}}"
+  labels:
+    app: "{{.Values.service.name}}"
+    aadpodidbinding: "{{.Values.service.name}}"
+spec:
+  concurrencyPolicy: "{{.Values.service.concurrencyPolicy}}"
+  schedule: "{{.Values.service.schedule}}"
+  jobTemplate:
+    spec:
+      template:
+        metadata:
+          name: "{{.Values.service.name}}"
+          labels:
+            app: "{{.Values.service.name}}"
+            aadpodidbinding: "{{.Values.service.name}}"
+        spec:
+          restartPolicy: "{{.Values.service.restartPolicy}}"
+```
