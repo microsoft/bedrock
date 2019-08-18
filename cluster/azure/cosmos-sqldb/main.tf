1e3f0473ca3c7e7142a70c93e7560db0b79a11fa
lingxd <lingxd@gmail.com>
2019-08-18 11:19:00 -0700

    "

--- cluster/azure/cosmos-sqldb/main.tf
+++ cluster/azure/cosmos-sqldb/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_cosmosdb_account" "cosmosdb_account" {
   consistency_policy {
     consistency_level       = "${var.consistency_level}"
     max_interval_in_seconds = 300
-    max_staleness_prefix    = 100010	    max_staleness_prefix    = 100000
+    max_staleness_prefix    = 100000
   }
 
   geo_location {
