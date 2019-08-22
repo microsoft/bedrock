78036e8cdf80c6c855162f658611cabd190fe880
xiaodoli <xiaodoli@microsoft.com>
2019-08-21 18:11:06 -0700

    fix deploy script

--- cluster/azure/aks-rbac/main.tf
+++ cluster/azure/aks-rbac/main.tf
@@ -6,7 +6,7 @@ resource "null_resource" "aks_roleassignment" {
   count = "${var.owners != "" || var.contributors != "" || var.readers != "" ? 1 : 0}"
 
   provisioner "local-exec" {
-    command = "echo 'Need to use this var so terraform waits for kubeconfig ' ${var.kubeconfigadmin_done};KUBECONFIG=${var.output_directory}/${var.kubeconfigadmin_filename} ${path.module}/apply_aad_rbac.sh -o ${var.owners}  -c ${var.contributors} -r ${var.readers}"
+    command = "echo 'Need to use this var so terraform waits for kubeconfig ' ${var.kubeconfigadmin_done};KUBECONFIG=${var.output_directory}/${var.kubeconfigadmin_filename} ${path.module}/apply_aad_rbac.sh -o \"${var.owners}\"  -c \"${var.contributors}\" -r \"${var.readers}\""
   }
 
   triggers = {
