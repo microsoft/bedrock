4fa765919bc27e9fbb4662d77d4e6480f324f068
lingxd <lingxd@gmail.com>
2019-08-23 23:24:55 -0700

    rm loc in aks

--- cluster/azure/aks-gitops/main.tf
+++ cluster/azure/aks-gitops/main.tf
@@ -6,7 +6,6 @@ module "aks" {
   source = "../../azure/aks"
 
   resource_group_name             = "${var.resource_group_name}"
-  resource_group_location         = "${var.resource_group_location}"
   cluster_name                    = "${var.cluster_name}"
   agent_vm_count                  = "${var.agent_vm_count}"
   agent_vm_size                   = "${var.agent_vm_size}"
