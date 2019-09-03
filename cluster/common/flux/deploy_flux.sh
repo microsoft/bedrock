a476032fb921bc6402e5fd71f8836557d210ff2b
xiaodoli <xiaodoli@microsoft.com>
2019-09-03 16:15:18 -0700

    singular

--- cluster/common/flux/deploy_flux.sh
+++ cluster/common/flux/deploy_flux.sh
@@ -61,7 +61,7 @@ fi
 #   git url: where flux monitors for manifests
 #   git ssh secret: kubernetes secret object for flux to read/write access to manifests repo
 echo "generating flux manifests with helm template"
-if ! helm template . --name "$RELEASE_NAME" --namespace "$KUBE_NAMESPACE" --values values.yaml --set image.repository="$FLUX_IMAGE_REPOSITORY" --set image.tag="$FLUX_IMAGE_TAG" --output-dir "./$FLUX_MANIFESTS" --set git.url="$GITOPS_SSH_URL" --set git.branch="$GITOPS_URL_BRANCH" --set git.secretName="$KUBE_SECRET_NAME" --set git.path="$GITOPS_PATH" --set git.pollInterval="$GITOPS_POLL_INTERVAL" --set registry.acr.enabled="$ACR_ENABLED" --set syncGarbageCollection.enabled="$GC_ENABLED" --set helmOperator.create="$CREATE_HELM_OPERATOR" --set helmOperator.createCRD="CREATE_CRDS" --set --git-labels="$GIT_LABELS"; then
+if ! helm template . --name "$RELEASE_NAME" --namespace "$KUBE_NAMESPACE" --values values.yaml --set image.repository="$FLUX_IMAGE_REPOSITORY" --set image.tag="$FLUX_IMAGE_TAG" --output-dir "./$FLUX_MANIFESTS" --set git.url="$GITOPS_SSH_URL" --set git.branch="$GITOPS_URL_BRANCH" --set git.secretName="$KUBE_SECRET_NAME" --set git.path="$GITOPS_PATH" --set git.pollInterval="$GITOPS_POLL_INTERVAL" --set registry.acr.enabled="$ACR_ENABLED" --set syncGarbageCollection.enabled="$GC_ENABLED" --set helmOperator.create="$CREATE_HELM_OPERATOR" --set helmOperator.createCRD="CREATE_CRDS" --set --git-label="$GIT_LABELS"; then
     echo "ERROR: failed to helm template"
     exit 1
 fi
