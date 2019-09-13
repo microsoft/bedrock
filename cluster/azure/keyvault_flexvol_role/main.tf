44e8b292980bacca31a20c2b2309a8946777a7f8
Xiaodong Li <xiaodoli@microsoft.com>
2019-09-13 11:23:24 -0700

    fix, role assignment use obj id

--- cluster/azure/keyvault_flexvol_role/main.tf
+++ cluster/azure/keyvault_flexvol_role/main.tf
@@ -2,7 +2,7 @@
 resource "azurerm_role_assignment" "flexvol" {
   count = "${var.enable_flexvol? 1 : 0}"
 
-  principal_id         = "${var.service_principal_id}"
+  principal_id         = "${var.service_principal_object_id}"
   role_definition_name = "${var.flexvol_role_assignment_role}"
   scope                = "/subscriptions/${var.subscription_id}/resourcegroups/${var.resource_group_name}/providers/Microsoft.KeyVault/vaults/${var.vault_name}"
 }
