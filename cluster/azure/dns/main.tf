fe063cf25e21ff3e8f521551a39d016931ebec40
Xiaodong Li <xiaodoli@microsoft.com>
2019-10-13 14:12:11 -0700

    fix role already exists

--- cluster/azure/dns/main.tf
+++ cluster/azure/dns/main.tf
@@ -2,32 +2,38 @@ module "azure-provider" {
   source = "../provider"
 }
 
-resource "azurerm_resource_group" "dnszone" {
-  name     = "${var.resource_group_name}"
-  location = "${var.location}"
-}
-
 resource "azurerm_dns_zone" "dnszone" {
-  name = "${var.name}"
-  resource_group_name = "${azurerm_resource_group.dnszone.name}"
-  zone_type = "Public"
+  name                = "${var.name}"
+  resource_group_name = "${var.resource_group_name}"
+  zone_type           = "Public"
 }
 
 resource "azurerm_dns_caa_record" "dnszone_caa" {
-  name = "caa"
-  zone_name = "${azurerm_dns_zone.dnszone.name}"
-  resource_group_name = "${azurerm_resource_group.dnszone.name}"
-  ttl = 300
+  name                = "caa"
+  zone_name           = "${azurerm_dns_zone.dnszone.name}"
+  resource_group_name = "${var.resource_group_name}"
+  ttl                 = 300
 
   record {
     flags = 0
-    tag = "issue"
+    tag   = "issue"
     value = "${var.caa_issuer}"
   }
+
+  depends_on = ["azurerm_dns_zone.dns_zone"]
 }
 
-resource "azurerm_role_assignment" "dnszone_contributor" {
-  scope = "${azurerm_dns_zone.dnszone.id}"
-  role_definition_name = "Contributor"
-  principal_id = "${var.service_principal_object_id}"
+resource "null_resource" "dnszone_contributor" {
+  count = "${var.name != "" && var.service_principal_object_id != "" ? 1 : 0}"
+
+  provisioner "local-exec" {
+    command = "${path.module}/add_role_assignment.sh -g ${var.resource_group_name} -z ${var.name} -o ${var.service_principal_object_id}"
+  }
+
+  triggers = {
+    name                        = "${var.name}"
+    service_principal_object_id = "${var.service_principal_object_id}"
+  }
+
+  depends_on = ["azurerm_dns_zone.dns_zone"]
 }
