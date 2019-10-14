0b8ce655397d8f7c00310cf27092ddcc426e5c45
xiaodoli <xiaodoli@microsoft.com>
2019-10-14 13:06:32 -0700

    defaults vars

--- cluster/azure/kube-recycle-pods/variables.tf
+++ cluster/azure/kube-recycle-pods/variables.tf
@@ -3,3 +3,19 @@ variable "k8s_namespace" {
   default = "default"
   description = "k8s namespace"
 }
+
+variable "kubeconfigadmin_filename" {
+  description = "Name of the admin kube config file saved to disk."
+  type        = "string"
+  default     = "admin_kube_config"
+}
+
+variable "kubeconfigadmin_done" {
+  description = "Allows flux to wait for the admin kubeconfig completion write to disk. Workaround for the fact that modules themselves cannot have dependencies."
+  type        = "string"
+}
+
+variable "output_directory" {
+  type    = "string"
+  default = "./output"
+}
