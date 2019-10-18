516992fb1956b8c2b43879fed10e3ea5a0c86618
xiaodoli <xiaodoli@microsoft.com>
2019-10-18 15:39:41 -0700

    trigger when changed

--- cluster/azure/cosmosdb-sp/main.tf
+++ cluster/azure/cosmosdb-sp/main.tf
@@ -1,8 +1,15 @@
-
 resource "null_resource" "deploy_stored_procedures" {
   count = "${var.sp_names != "" && var.collection_name != "" ? 1 : 0}"
 
   provisioner "local-exec" {
-    command = "pwsh ${path.module}/ensure_cosmosdb_sp.ps1 -AccountName ${var.cosmos_db_account} -DbName ${var.cosmos_db_name} -CollectionName \"${var.cosmos_db_collection}\" -SpNames \"${var.sp_names}\" -VaultName ${var.cosmos_db_sp_names}"
+    command = "pwsh ${path.module}/ensure_cosmosdb_sp.ps1 -AccountName ${var.cosmos_db_account} -DbName ${var.cosmos_db_name} -CollectionName \"${var.cosmos_db_collection}\" -SpNames \"${var.cosmos_db_sp_names}\" -VaultName ${var.cosmos_db_sp_names}"
+  }
+
+  triggers = {
+    cosmos_db_account     = "${var.cosmos_db_account}"
+    cosmos_db_name        = "${var.cosmos_db_name}"
+    cosmos_db_collection  = "${var.cosmos_db_collection}"
+    cosmos_db_sp_names    = "${var.cosmos_db_sp_names}"
+    cosmos_db_sp_versions = "${var.cosmos_db_sp_versions}"
   }
 }
--- cluster/azure/cosmosdb-sp/variables.tf
+++ cluster/azure/cosmosdb-sp/variables.tf
@@ -19,6 +19,11 @@ variable "cosmos_db_sp_names" {
   description = "list of sp names are separated by ','"
 }
 
+variable "cosmos_db_sp_versions" {
+  type = "string"
+  description = "list of kv versions for sp definitions, will trigger new deployment when it got changed"
+}
+
 variable "vault_name" {
   type        = "string"
   description = "key vault to store auth key of cosmosdb connection"
