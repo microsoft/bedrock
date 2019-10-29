084341f43bda5f3961882f0ed418cf4e333935c4
xiaodoli <xiaodoli@microsoft.com>
2019-10-28 17:02:48 -0700

    rm dup var

--- cluster/azure/app-insights/variables.tf
+++ cluster/azure/app-insights/variables.tf
@@ -37,8 +37,3 @@ variable "subscription_id" {
   type = "string"
   description = "azure subscription id where app insights is created"
 }
-
-variable "resource_group_name" {
-type = "string"
-  description = "azure resource group where app insights is created"
-}
