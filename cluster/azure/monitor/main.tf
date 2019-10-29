71e7a616027f229be15afe9ca1eace345e73d373
Xiaodong Li <xiaodoli@microsoft.com>
2019-10-29 02:46:48 -0700

    rm readonly field fromd ata

--- cluster/azure/monitor/main.tf
+++ cluster/azure/monitor/main.tf
@@ -38,9 +38,7 @@ resource "azurerm_monitor_action_group" "sms-alert" {
 
 data "azurerm_application_insights" "app_insights" {
   name                = "${var.app_insights_name}"
-  location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
-  application_type    = "web"
 }
 
 resource "azurerm_monitor_metric_alert" "unhandled_exception_sev3" {
