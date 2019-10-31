e4559758bf2221d0eaa19a3af2409969e816d9b9
xiaodoli <xiaodoli@microsoft.com>
2019-10-30 17:31:46 -0700

    wrong var name: subscription_id

--- cluster/azure/traffic-manager/main.tf
+++ cluster/azure/traffic-manager/main.tf
@@ -10,12 +10,12 @@ resource "null_resource" "traffic_manager" {
   count = "${var.traffic_manager_name != "" && var.resource_group_name != "" ? 1 : 0}"
 
   provisioner "local-exec" {
-    command = "${path.module}/create_traffic_manager.sh -s ${var.dns_subscription_id} -g ${var.resource_group_name} -t ${var.traffic_manager_name} -e \"${var.service_names}\" -u \"${var.service_suffix}\" -z ${var.dns_zone_name} -p \"${var.probe_path}\""
+    command = "${path.module}/create_traffic_manager.sh -s ${var.subscription_id} -g ${var.resource_group_name} -t ${var.traffic_manager_name} -e \"${var.service_names}\" -u \"${var.service_suffix}\" -z ${var.dns_zone_name} -p \"${var.probe_path}\""
   }
 
   triggers = {
     traffic_manager_name = "${var.traffic_manager_name}"
-    dns_subscription_id  = "${var.dns_subscription_id}"
+    subscription_id      = "${var.subscription_id}"
     resource_group_name  = "${var.resource_group_name}"
     service_names        = "${var.service_names}"
     service_suffix       = "${var.service_suffix}"
