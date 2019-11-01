f4a49269e1838da74d78a92e488e5bcdad05411d
Xiaodong Li <xiaodoli@microsoft.com>
2019-11-01 01:29:16 -0700

    unable to set empty string, use "empty"

--- cluster/azure/traffic-manager/main.tf
+++ cluster/azure/traffic-manager/main.tf
@@ -7,7 +7,7 @@ provider "azurerm" {
 }
 
 resource "null_resource" "traffic_manager" {
-  count = "${var.traffic_manager_name != "" && var.resource_group_name != "" ? 1 : 0}"
+  count = "${var.traffic_manager_name != "" && var.traffic_manager_name != "empty" && var.resource_group_name != "" ? 1 : 0}"
 
   provisioner "local-exec" {
     command = "${path.module}/create_traffic_manager.sh -s ${var.subscription_id} -g ${var.resource_group_name} -t ${var.traffic_manager_name} -e \"${var.service_names}\" -u \"${var.service_suffix}\" -z \"${var.dns_zone_name}\" -p \"${var.probe_path}\" -l \"${var.location}\""
