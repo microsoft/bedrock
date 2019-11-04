805de1f40f74943aa44a1de766e6b107517cf33d
xiaodoli <xiaodoli@microsoft.com>
2019-11-04 13:03:27 -0800

    check adf existance before invoking trigger

--- cluster/azure/adf/stop-adf-triggers.ps1
+++ cluster/azure/adf/stop-adf-triggers.ps1
@@ -5,11 +5,23 @@ param(
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
-$triggers = az rest --method GET --uri "https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/$ResourceGroupName/providers/Microsoft.DataFactory/factories/$AdfName/triggers?api-version=2018-06-01" | ConvertFrom-Json
+$adfCreated = $false
+[array]$existingAdfs = az rest --method GET --uri "https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/$ResourceGroupName/providers/Microsoft.DataFactory/factories?api-version=2018-06-01" | ConvertFrom-Json
+if ($null -ne $existingAdfs -and $existingAdfs.Count -gt 0) {
+    $adf = $_
+    Write-Host $adf
+    if ($adf.name -eq $AdfName) {
+        $adfCreated = $true
+    }
+}
 
-$triggers.value | ForEach-Object {
-    $triggerName = $_.name
-    Write-Host "Found trigger with name: $triggerName"
-    Write-Host "Disabling trigger with name: $triggerName"
-    az rest --method POST --uri "https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/$ResourceGroupName/providers/Microsoft.DataFactory/factories/$AdfName/triggers/$triggerName/stop?api-version=2018-06-01"
+if ($adfCreated) {
+    $triggers = az rest --method GET --uri "https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/$ResourceGroupName/providers/Microsoft.DataFactory/factories/$AdfName/triggers?api-version=2018-06-01" | ConvertFrom-Json
+
+    $triggers.value | ForEach-Object {
+        $triggerName = $_.name
+        Write-Host "Found trigger with name: $triggerName"
+        Write-Host "Disabling trigger with name: $triggerName"
+        az rest --method POST --uri "https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/$ResourceGroupName/providers/Microsoft.DataFactory/factories/$AdfName/triggers/$triggerName/stop?api-version=2018-06-01"
+    }
 }
