33ef9d94501da2f21e7305b20275d02c1617b9b1
Xiaodong Li <xiaodoli@microsoft.com>
2019-11-09 01:34:26 -0800

    no need for output

--- cluster/azure/app-insights/output.tf
+++
@@ -1,7 +0,0 @@
-output "instrumentation_key" {
-  value = "${azurerm_application_insights.app_insights.instrumentation_key}"
-}
-
-output "app_id" {
-  value = "${azurerm_application_insights.app_insights.app_id}"
-}
