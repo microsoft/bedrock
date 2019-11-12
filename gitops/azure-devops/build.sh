920c3da7278c44dc9b1e5f50702c597346e9d15e
Michael Tarng <20913254+mtarng@users.noreply.github.com>
2019-11-11 16:51:28 -0800

    Update build.sh

--- gitops/azure-devops/build.sh
+++ gitops/azure-devops/build.sh
@@ -144,10 +144,10 @@ function get_spk_version() {
     if [ -z "$VERSION" ]
     then
         # By default, the script will use the most recent non-prerelease, non-draft release SPK
-        VERSION_TO_DOWNLOAD=$(curl -s "https://api.github.com/repos/CatalystCode/spk/releases/latest" | grep "tag_name" | sed -E 's/.*"([^"]+)".*/\1/')
+        SPK_VERSION_TO_DOWNLOAD=$(curl -s "https://api.github.com/repos/CatalystCode/spk/releases/latest" | grep "tag_name" | sed -E 's/.*"([^"]+)".*/\1/')
     else
         echo "SPK Version: $VERSION"
-        VERSION_TO_DOWNLOAD=$VERSION
+        SPK_VERSION_TO_DOWNLOAD=$VERSION
     fi
 }
 
@@ -167,16 +167,16 @@ function get_os_spk() {
 # Download SPK
 function download_spk() {
     echo "DOWNLOADING SPK"
-    echo "Latest SPK Version: $VERSION_TO_DOWNLOAD"
+    echo "Latest SPK Version: $SPK_VERSION_TO_DOWNLOAD"
     os=''
     get_os_spk os
-    spk_wget=$(wget -SO- "https://github.com/CatalystCode/spk/releases/download/$VERSION_TO_DOWNLOAD/spk-$os" 2>&1 | grep -E -i "302")
+    spk_wget=$(wget -SO- "https://github.com/CatalystCode/spk/releases/download/$SPK_VERSION_TO_DOWNLOAD/spk-$os" 2>&1 | grep -E -i "302")
     if [[ $spk_wget == *"302 Found"* ]]; then
-    echo "SPK $VERSION_TO_DOWNLOAD downloaded successfully."
+    echo "SPK $SPK_VERSION_TO_DOWNLOAD downloaded successfully."
     else
         echo "There was an error when downloading SPK. Please check version number and try again."
     fi
-    wget "https://github.com/CatalystCode/spk/releases/download/$VERSION_TO_DOWNLOAD/spk-$os"
+    wget "https://github.com/CatalystCode/spk/releases/download/$SPK_VERSION_TO_DOWNLOAD/spk-$os"
     mkdir spk
     mv spk-$os spk/spk
     chmod +x spk/spk 
