75f80445ed7fef8ded28ff18acd63e521d79159d
edaena <edaena@outlook.com>
2019-11-21 11:11:26 +0100

    Add git_label variable to minikube deploy script

--- cluster/environments/minikube/deploy_minikube.sh
+++ cluster/environments/minikube/deploy_minikube.sh
@@ -6,6 +6,7 @@ GITOPS_SSH_BRANCH="master"
 GITOPS_SSH_KEY="{insert the path to the private ssh key file here}"
 FLUX_REPO_URL="https://github.com/weaveworks/flux.git"
 REPO_ROOT_DIR="repo-root"
+GIT_LABEL="flux-sync"
 
 # install flux into cluster
-./../../common/flux/deploy_flux.sh -g "$GITOPS_SSH_URL" -b "$GITOPS_SSH_BRANCH" -k "$GITOPS_SSH_KEY" -f "$FLUX_REPO_URL" -d "$REPO_ROOT_DIR"
+./../../common/flux/deploy_flux.sh -g "$GITOPS_SSH_URL" -b "$GITOPS_SSH_BRANCH" -k "$GITOPS_SSH_KEY" -f "$FLUX_REPO_URL" -d "$REPO_ROOT_DIR" -l "$GIT_LABEL"
