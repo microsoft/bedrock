f4d008f0403e2a5625721e12b0e1c0f43c34898f
Jim Spring <jmspring@gmail.com>
2019-12-09 21:21:55 +0000

    clean up helm setup

--- tools/prereqs/setup_helm.sh
+++ tools/prereqs/setup_helm.sh
@@ -2,11 +2,18 @@
 
 HELM_DESIRED_VERSION="v2.16.1"
 
-# verify we are running as root
-if [[ "$EUID" != 0 ]]; then
-    echo "Script must be run as root or sudo."
-    exit 1
-fi
+# load common functions
+SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
+. $SCRIPT_DIR/common_funcs.sh
+
+require_root
+
+function finish {
+    if [ ! -z "$tmp_dir" ]; then
+        rm -rf $tmp_dir
+    fi
+}
+trap finish EXIT
 
 # prompt for confirmation
 echo "This script will install version $HELM_DESIRED_VERSION of helm from Github."
@@ -23,12 +30,8 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 cd $tmp_dir
 
 # retrieve and install helm
-
 # Currently, Bedrock only works with Helm 2.x, so specifying a specific version
 curl -LO https://git.io/get_helm.sh
 chmod 700 ./get_helm.sh
 ./get_helm.sh --version $HELM_DESIRED_VERSION
-
-# clean up
 cd -
-rm -rf $tmp_dir
