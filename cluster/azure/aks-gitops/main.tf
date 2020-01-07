96be48daac4f7eb53eb93d5d02a6ca15132a3d59
Sarath Pinninty <sarathp@microsoft.com>
2020-01-07 11:45:28 -0800

    Fixes kube config output writing to console (#853)
    * Fixes #802
    
    * Added doc to redownload the credentials
    
    * Reverting module sources

--- cluster/azure/aks-gitops/main.tf
+++ cluster/azure/aks-gitops/main.tf
@@ -17,7 +17,6 @@ module "aks" {
   service_cidr             = "${var.service_cidr}"
   dns_ip                   = "${var.dns_ip}"
   docker_cidr              = "${var.docker_cidr}"
-  kubeconfig_recreate      = "${var.kubeconfig_recreate}"
   kubeconfig_filename      = "${var.kubeconfig_filename}"
   network_policy           = "${var.network_policy}"
   network_plugin           = "${var.network_plugin}"
