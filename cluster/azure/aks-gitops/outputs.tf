52303ee011587869a2aa264fb98a158e85d47fb3
erikschlegel <erik.schlegel@gmail.com>
2020-02-12 16:14:02 -0600

    adding system assigned identity outputs

--- cluster/azure/aks-gitops/outputs.tf
+++ cluster/azure/aks-gitops/outputs.tf
@@ -6,6 +6,10 @@ output "aks_flux_kubediff_done" {
   value = "${module.aks.kubeconfig_done}_${module.flux.flux_done}_${module.kubediff.kubediff_done}"
 }
 
+output "aks_resource_id" {
+  value = module.aks.resource_id
+}
+
 output "aks_msi_principal_id" {
   value = module.aks.msi_principal_id
 }
--- cluster/azure/aks/outputs.tf
+++ cluster/azure/aks/outputs.tf
@@ -12,6 +12,10 @@ output "kubeconfig_done" {
   value = join("", local_file.cluster_credentials.*.id)
 }
 
+output "resource_id" {
+  value = azurerm_kubernetes_cluster.cluster.id
+}
+
 output "msi_principal_id" {
   value = azurerm_kubernetes_cluster.cluster.identity.principal_id
 }
