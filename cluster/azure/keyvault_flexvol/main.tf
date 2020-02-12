953fb4985419a9fbae5fc94cf92b5e3f39fb573d
erikschlegel <erik.schlegel@gmail.com>
2020-02-11 21:07:27 -0600

    removing deprecated vault_name property

--- cluster/azure/keyvault_flexvol/main.tf
+++ cluster/azure/keyvault_flexvol/main.tf
@@ -15,7 +15,6 @@ resource "azurerm_key_vault_access_policy" "flexvol" {
   count = var.enable_flexvol ? 1 : 0
 
   key_vault_id        = data.azurerm_key_vault.kv.id
-  resource_group_name = var.resource_group_name
 
   tenant_id = var.tenant_id
   object_id = data.azuread_service_principal.flexvol.id
