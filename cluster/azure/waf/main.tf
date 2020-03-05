97fbb565676f415092c4bb3e7fe619940ce78279
nrose <rose.nathanielm@gmail.com>
2020-03-05 13:33:30 -0800

    removing syntax errors in Waf template

--- cluster/azure/waf/main.tf
+++ cluster/azure/waf/main.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "wafrg" {
 }
 
 resource "azurerm_application_gateway" "waf" {
-  name                = "${var.wafname}-waf"
+  name                = var.wafname-waf
   resource_group_name = data.azurerm_resource_group.wafrg.name
   location            = data.azurerm_resource_group.wafrg.location
 
@@ -27,26 +27,26 @@ resource "azurerm_application_gateway" "waf" {
   }
 
   frontend_port {
-    name = "${var.prefix}-feport443"
+    name = var.prefix-feport443
     port = 443
   }
 
   frontend_port {
-    name = "${var.prefix}-feport"
+    name = var.prefix-feport
     port = 80
   }
 
   frontend_ip_configuration {
-    name                 = "${var.prefix}-feip"
+    name                 = var.prefix-feip
     public_ip_address_id = var.public_ip_address_id
   }
 
   backend_address_pool {
-    name = var.prefix}-beappool1"
+    name = var.prefix}-beappool1
   }
 
   backend_http_settings {
-    name                  = "${var.prefix}-httpsetting1"
+    name                  = var.prefix-httpsetting1
     cookie_based_affinity = "Disabled"
     port                  = 80
     protocol              = "Http"
@@ -54,17 +54,17 @@ resource "azurerm_application_gateway" "waf" {
   }
 
   http_listener {
-    name                           = "${var.prefix}-httplstn1"
-    frontend_ip_configuration_name = "${var.prefix}-feip"
-    frontend_port_name             = "${var.prefix}-feport"
+    name                           = var.prefix-httplstn1
+    frontend_ip_configuration_name = var.prefix-feip
+    frontend_port_name             = var.prefix-feport
     protocol                       = "Http"
   }
 
   request_routing_rule {
-    name                       = "${var.prefix}-rule1"
+    name                       = var.prefix-rule1
     rule_type                  = "Basic"
-    http_listener_name         = "${var.prefix}-httplstn1"
-    backend_address_pool_name  = "${var.prefix}-beappool1"
-    backend_http_settings_name = "${var.prefix}-httpsetting1"
+    http_listener_name         = var.prefix-httplstn1
+    backend_address_pool_name  = var.prefix-beappool1
+    backend_http_settings_name = var.prefix-httpsetting1
   }
 }
