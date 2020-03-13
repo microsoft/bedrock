8f5586c8a6c38804f88c7e68e4ab7b78f6929912
erikschlegel <erik.schlegel@gmail.com>
2020-03-12 21:39:43 -0500

    Adding kubelet identity

--- cluster/azure/keyvault_flexvol/outputs.tf
+++ cluster/azure/keyvault_flexvol/outputs.tf
@@ -1,11 +1,11 @@
 output "aks_kv_identity_id" {
-  value = data.azurerm_user_assigned_identity.aks_kv_user_identity.id
+  value = azurerm_user_assigned_identity.aks_kv_user_identity.id
 }
 
 output "aks_kv_identity_principal_id" {
-  value = data.azurerm_user_assigned_identity.aks_kv_user_identity.principal_id
+  value = azurerm_user_assigned_identity.aks_kv_user_identity.principal_id
 }
 
 output "aks_kv_identity_client_id" {
-  value = data.azurerm_user_assigned_identity.aks_kv_user_identity.client_id
+  value = azurerm_user_assigned_identity.aks_kv_user_identity.client_id
 }
