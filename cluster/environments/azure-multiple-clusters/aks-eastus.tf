fb6581acf664cf7d0e562c81e746d305118dcd08
jim spring <jmspring@gmail.com>
2020-03-25 19:41:32 +0000

    need relative path

--- cluster/environments/azure-multiple-clusters/aks-eastus.tf
+++ cluster/environments/azure-multiple-clusters/aks-eastus.tf
@@ -36,7 +36,8 @@ module "east_subnet" {
 
 # Creates east aks cluster, flux, kubediff
 module "east_aks_gitops" {
-  source = "github.com/microsoft/bedrock?ref=master//cluster/azure/aks-gitops"
+  #source = "github.com/microsoft/bedrock?ref=master//cluster/azure/aks-gitops"
+  source = "../../../cluster/azure/aks-gitops"
 
   acr_enabled              = var.acr_enabled
   agent_vm_count           = var.agent_vm_count
