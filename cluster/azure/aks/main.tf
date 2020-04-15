77400f5e494b2c394744f11fc775fa13d8c753ee
Erik Schlegel <erisch@microsoft.com>
2020-04-15 16:05:28 -0500

    fixed aks bug

--- cluster/azure/aks/main.tf
+++ cluster/azure/aks/main.tf
@@ -80,7 +80,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
       client_id     = var.service_principal_id
       client_secret = var.service_principal_secret
     }] : []
-    content = {
+    content {
       client_id     = value.client_id
       client_secret = value.client_secret
     }
