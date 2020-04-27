4c737c8d3093110d9d0eac4e0a64261c5ffd8eee
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2020-04-27 13:17:47 -0700

    changing absolute to relative paths to pass integration tests

--- cluster/environments/azure-multiple-clusters/trafficmanager.tf
+++ cluster/environments/azure-multiple-clusters/trafficmanager.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "tmrg" {
 }
 
 module "trafficmanager" {
-  source = "github.com/microsoft/bedrock?ref=master//cluster/azure/tm-profile"
+  source = "../../azure/tm-profile"
 
   resource_group_name              = data.azurerm_resource_group.tmrg.name
   traffic_manager_profile_name     = var.traffic_manager_profile_name
