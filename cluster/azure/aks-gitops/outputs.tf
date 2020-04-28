668d721c86a437f283e6e6d2e015e01516203a0d
Erik Schlegel <erik.schlegel@gmail.com>
2020-04-28 13:34:46 -0500

    Enable MSI Support for aks-gitops module (#1368)
    * Adding msi integration
    
    * Adding msi integration
    
    * Adding msi integration
    
    * Adding msi integration
    
    * Adding msi integration
    
    * Adding msi integration
    
    * Adding msi integration
    
    * Adding msi integration
    
    * Adding msi integration
    
    * Adding msi integration
    
    * Adding msi integration
    
    * Adding msi integration
    
    * rolling back terraform version change
    
    * Adding aks resource id to output
    
    * removing agent_pool_profile which is now considered EOL
    
    * removing deprecated vault_name property
    
    * removing deprecated vault_name property
    
    * removing deprecated vault_name property
    
    * removing deprecated vault_name property
    
    * Adding node_count
    
    * Adding msi_enabled var to aks-gitops module
    
    * adding system assigned identity outputs
    
    * adding system assigned identity outputs
    
    * adding system assigned identity outputs
    
    * exporting client id through data external script
    
    * Adding subscription is
    
    * Adding subscription is
    
    * removing tenant id output
    
    * Adding kubelet identity
    
    * Adding kubelet identity
    
    * Adding kubelet identity
    
    * Adding kubelet identity
    
    * Adding kubelet identity
    
    * Adding kubelet identity
    
    * Adding kubelet identity
    
    * Adding kubelet resource id
    
    * Adding kubelet resource id
    
    * refactoring aks mod to create use assigned identity
    
    * refactoring aks mod to create use assigned identity
    
    * refactoring aks mod to create use assigned identity
    
    * removing kubelet identity default
    
    * Adding vnet subnet id
    
    * version bump
    
    * creating dynamic block for sp provision
    
    * version bump
    
    * fixed aks bug
    
    * fixed aks bug
    
    * running dos2unix
    
    * adding agent pool resource id to output
    
    * adding agent pool resource id to output
    
    * adding agent pool resource id to output
    
    * adding agent pool resource id to output
    
    * rolling back version change
    
    * removing user identity setup and adding node resource group export
    
    * reverting flexvol changes
    
    * adding nelwine
    
    * Adding condition to support aks auto generating sp if sp client id isn't specified
    
    * reverting windows profile change
    
    * Adding sp terraform variables as optional in aks-gitops module
    
    * Adding newline
    
    * fixing node group export bug
    
    * fixing node group export bug
    
    * changing script execution permisssions

--- cluster/azure/aks-gitops/outputs.tf
+++ cluster/azure/aks-gitops/outputs.tf
@@ -5,3 +5,27 @@ output "kubeconfig_done" {
 output "aks_flux_kubediff_done" {
   value = "${module.aks.kubeconfig_done}_${module.flux.flux_done}_${module.kubediff.kubediff_done}"
 }
+
+output "aks_resource_id" {
+  value = module.aks.resource_id
+}
+
+output "msi_client_id" {
+  value = module.aks.msi_client_id
+}
+
+output "kubelet_client_id" {
+  value = module.aks.kubelet_client_id
+}
+
+output "kubelet_id" {
+  value = module.aks.kubelet_id
+}
+
+output "kubelet_resource_id" {
+  value = module.aks.kubelet_resource_id
+}
+
+output "node_resource_group" {
+  value = module.aks.node_resource_group
+}
