fb46fbe311999f0445f7a7f88da02c3e609e567a
Nate <NathanielRose@users.noreply.github.com>
2020-04-29 11:38:46 -0700

    [WIP] Adjusting modules to relative paths (#1189)
    * adjusting modules to relative paths
    
    * adding build badge

--- cluster/environments/azure-single-keyvault/main.tf
+++ cluster/environments/azure-single-keyvault/main.tf
@@ -26,7 +26,7 @@ module "subnet" {
 }
 
 module "aks-gitops" {
-  source = "github.com/microsoft/bedrock?ref=master//cluster/azure/aks-gitops"
+  source = "../../azure/aks-gitops"
 
   acr_enabled              = var.acr_enabled
   agent_vm_count           = var.agent_vm_count
@@ -54,7 +54,7 @@ module "aks-gitops" {
 
 # Create Azure Key Vault role for SP
 module "keyvault_flexvolume_role" {
-  source = "github.com/microsoft/bedrock?ref=master//cluster/azure/keyvault_flexvol_role"
+  source = "../../azure/keyvault_flexvol_role"
 
   resource_group_name  = data.azurerm_resource_group.keyvault.name
   service_principal_id = var.service_principal_id
@@ -64,7 +64,7 @@ module "keyvault_flexvolume_role" {
 
 # Deploy central keyvault flexvolume
 module "flex_volume" {
-  source = "github.com/microsoft/bedrock?ref=master//cluster/azure/keyvault_flexvol"
+  source = "../../azure/keyvault_flexvol"
 
   resource_group_name      = data.azurerm_resource_group.keyvault.name
   service_principal_id     = var.service_principal_id
